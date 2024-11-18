0ee7fd180e25dc54db7988da138fb490530f4ab2
Sandra PC <119578353+Sandrapc1@users.noreply.github.com>
2024-11-18 18:59:05 +0530

    Update user_list_screen.dart
    remove comment code

--- lib/screens/user_list_screen.dart
+++ lib/screens/user_list_screen.dart
@@ -1,35 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:provider/provider.dart';
-// import '../state/user_provider.dart';
-// import '../widgets/user_card.dart';
-
-// class UserListScreen extends StatelessWidget {
-//   const UserListScreen({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     final userProvider = Provider.of<UserProvider>(context);
-
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('User List'),
-//       ),
-//       body: RefreshIndicator(
-//         onRefresh: userProvider.fetchUsers,
-//         child: userProvider.isLoading
-//             ? const Center(child: CircularProgressIndicator())
-//             : userProvider.error != null
-//                 ? Center(child: Text(userProvider.error!))
-//                 : ListView.builder(
-//                     itemCount: userProvider.users.length,
-//                     itemBuilder: (context, index) {
-//                       return UserCard(user: userProvider.users[index]);
-//                     },
-//                   ),
-//       ),
-//     );
-//   }
-// }
 
 
 import 'package:flutter/material.dart';
